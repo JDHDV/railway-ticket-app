462b551e0b611cec86d3cf15ab2b668282272da7
JDHDV <1073991028@qq.com>
2018-01-09 11:27:20 +0800

    完善底部导航‘车票预订’等样式

--- src/component/navlink/navlink.js
+++ src/component/navlink/navlink.js
@@ -13,6 +13,7 @@ class NavLink extends React.Component{
 		this.handleAbout=this.handleAbout.bind(this);
 		this.handleTicket=this.handleTicket.bind(this);
 		this.handleBusiness=this.handleBusiness.bind(this);
+		this.handleChange=this.handleChange.bind(this);
 	}
 	handleTicket(){
 		this.props.history.push("/ticketsearch");
@@ -26,20 +27,26 @@ class NavLink extends React.Component{
 	handleBook(){
 		this.props.history.push("/searchbook");
 	}
+	handleChange(e){
+		this.bookTicket.className="am-flexbox-item";
+		this.busiService.className="am-flexbox-item";
+		this.searchBook.className="am-flexbox-item";
+		this.aboutMe.className="am-flexbox-item";
+		e.target.className="am-flexbox-item current";
+	}
 	render(){
 		return(
 			<div className="footer-navlink">
 				<List>
 					<Flex>
-						<Flex.Item onClick={this.handleTicket}>车票预订</Flex.Item>
-						<Flex.Item onClick={this.handleBusiness}>商旅服务</Flex.Item>
-						<Flex.Item onClick={this.handleBook}>订票查询</Flex.Item>
-						<Flex.Item onClick={this.handleAbout}>关于我的</Flex.Item>
+						<Flex.Item onTouchEnd={this.handleTicket} onClick={this.handleChange} ref={(bookTicket=>{this.bookTicket=bookTicket})}>车票预订</Flex.Item>
+						<Flex.Item  onTouchEnd={this.handleBusiness} onClick={this.handleChange} ref={(busiService=>{this.busiService=busiService})}>商旅服务</Flex.Item>
+						<Flex.Item  onTouchEnd={this.handleBook} onClick={this.handleChange} ref={(searchBook=>{this.searchBook=searchBook})}>订票查询</Flex.Item>
+						<Flex.Item  onTouchEnd={this.handleAbout} onClick={this.handleChange} ref={(aboutMe=>{this.aboutMe=aboutMe})}>关于我的</Flex.Item>
 					</Flex>
 				</List>
 			</div>
 		)
 	}
 }
-
 export default NavLink;
